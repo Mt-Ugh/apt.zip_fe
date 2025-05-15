f9f209f74bc1fea2593fa49e323b23e526c6a10c
unknown <skes01149@naver.com>
2025-05-15 17:58:36 +0900

    feat: Footer 추가

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Header from '@/components/layout/Header.vue'
+import Footer from '@/components/layout/Footer.vue'
 </script>
 
 <style>
