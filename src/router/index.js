26dce54a6c1f38b6dddcd333aaf0dcc833251196
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2025-05-19 11:36:19 +0900

    feat: 로그인, 회원가입 화면 구성 및 API 연동 (#7)
    * feat: 로그인, 회원가입 화면 구현 및 API 연동
    
    * feat: 구글 로그인 기능 추가
    
    * feat: 로그인 정보 저장 및 토큰 재발급 기능 추가
    
    * feat: 공용 모달 추가
    
    * feat: 로그아웃 기능 추가
    
    * feat: 전화번호 유효성 검사 추가 및 코드 정리
    
    * style: 불필요한 코드 제거

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AuthView from '@/views/Auth/AuthView.vue'
+import AuthCallback from '@/views/Auth/AuthCallback.vue'
 
-const routes = [{}]
+const routes = [
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: AuthView,
+  },
+  {
+    path: '/auth/callback',
+    name: 'AuthCallback',
+    component: AuthCallback,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
