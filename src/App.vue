dcf0e93a071cbfe79c8faa0f52ba290bc665a2df
HwanSu Kim <68542769+KimHS17@users.noreply.github.com>
2025-05-26 16:58:34 +0900

    chore: main브랜치 변경사항 deploy브랜치로 병합 (#38)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <Header :isHome="isHomePage" />
     <router-view />
-    <ChatBot /> 
+    <ChatBot />
     <Footer />
   </div>
 </template>
@@ -13,7 +13,7 @@ import { useRoute } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import Header from '@/components/layout/Header.vue'
 import Footer from '@/components/layout/Footer.vue'
-import ChatBot from '@/components/layout/Chatbot.vue'; 
+import ChatBot from '@/components/layout/ChatBot.vue'
 
 const route = useRoute()
 const isHomePage = computed(() => route.path === '/')
